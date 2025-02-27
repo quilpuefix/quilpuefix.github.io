2def3c8b31630169d0ff6ed867511e2eede1e139
QuilpueFix <93630978+quilpuefix@users.noreply.github.com>
2025-02-27 03:31:23 -0300

    Delete QuilpuéFix directory

--- QuilpuéFix/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const faqItems = document.querySelectorAll(".faq-item");
-
-    faqItems.forEach(item => {
-        item.querySelector(".faq-question").addEventListener("click", function () {
-            item.classList.toggle("active");
-        });
-    });
-});

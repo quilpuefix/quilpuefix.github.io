5ffeb7739d0a4befb91a255a997ecdeceadea344
QuilpueFix <93630978+quilpuefix@users.noreply.github.com>
2025-02-27 03:30:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const container = document.querySelector(".service-container");
-    const btnLeft = document.querySelector(".scroll-btn.left");
-    const btnRight = document.querySelector(".scroll-btn.right");
+    const faqItems = document.querySelectorAll(".faq-item");
 
-    if (!container || !btnLeft || !btnRight) {
-        console.error("Uno o más elementos no fueron encontrados.");
-        return;
-    }
-
-    const scrollAmount = 300;
-
-    btnLeft.onclick = () => {
-        console.log("Botón izquierdo clickeado");
-        container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
-    };
-
-    btnRight.onclick = () => {
-        console.log("Botón derecho clickeado");
-        container.scrollBy({ left: scrollAmount, behavior: "smooth" });
-    };
+    faqItems.forEach(item => {
+        item.querySelector(".faq-question").addEventListener("click", function () {
+            item.classList.toggle("active");
+        });
+    });
 });

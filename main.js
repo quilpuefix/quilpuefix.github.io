e10eb04cccf49e8d02f5f72f5583aa6325f259f5
QuilpueFix <93630978+quilpuefix@users.noreply.github.com>
2025-04-05 14:10:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,3 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+        // Función para mostrar y ocultar el menú en móviles
+        document.getElementById("mobile-menu-button").addEventListener("click", function() {
+            const menu = document.getElementById("mobile-menu");
+            menu.classList.toggle("hidden"); // Alterna la clase 'hidden' para mostrar/ocultar el menú
+          });
